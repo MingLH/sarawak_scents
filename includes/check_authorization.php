a06f4f8659b361aafbe3bfbf372f001b1d2036ec
Javin Sim <javinsim04@gmail.com>
2025-12-17 15:38:24 +0800

    Update check_authorization.php to fix redirect error

--- includes/check_authorization.php
+++ includes/check_authorization.php
@@ -3,7 +3,7 @@ session_start();
 
 //to prevent user/admin from accesing page when they're not logging in
 if (isset($_SESSION['user_id'])) {
-    $target = ($_SESSION['role'] === 'admin') ? 'admin_dashboard.php' : 'index.php';
+    $target = ($_SESSION['role'] === 'admin') ? 'admin/admin_dashboard.php' : 'index.php';
     header("Location: $target");
     exit();
 }
