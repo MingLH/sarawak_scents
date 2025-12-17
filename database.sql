708e0f18e4e287373d6b3938834466fd81c32497
Javin Sim <javinsim04@gmail.com>
2025-12-17 12:28:12 +0800

    change the attributes name for 'phone' to 'phone_number'

--- database.sql
+++ database.sql
@@ -8,10 +8,10 @@ CREATE TABLE IF NOT EXISTS users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL, 
+    phone_number VARCHAR(20), 
     role ENUM('member', 'admin') DEFAULT 'member',
     address TEXT,
-    phone VARCHAR(20),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
