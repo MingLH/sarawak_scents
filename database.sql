cf278be5aede7ca49f47f82fb19d44ef40a36460
Javin Sim <javinsim04@gmail.com>
2025-12-18 13:48:18 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,76 @@
+-- Project: Sarawak Scents
 
+CREATE DATABASE IF NOT EXISTS sarawak_scents_db;
+USE sarawak_scents_db;
+
+-- User Table
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL, 
+    phone_number VARCHAR(20),
+    otp_code VARCHAR(10) DEFAULT NULL,
+    role ENUM('member', 'admin') DEFAULT 'member',
+    address TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Categories Table
+CREATE TABLE IF NOT EXISTS categories (
+    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL
+);
+
+-- Categories
+INSERT INTO categories (category_name) VALUES
+('Perfume'),
+('Soap'),
+('Candle');
+
+-- Products Table
+CREATE TABLE IF NOT EXISTS products (
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    description TEXT,
+    price DECIMAL(10,2) NOT NULL,
+    image VARCHAR(255),
+    category_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+);
+
+-- Orders Table
+CREATE TABLE IF NOT EXISTS orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    total_amount DECIMAL(10,2) NOT NULL,
+    order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status ENUM('Pending', 'Paid', 'Cancelled') DEFAULT 'Pending',
+
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+-- Cart Tabble
+CREATE TABLE IF NOT EXISTS order_items (
+    order_item_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    quantity INT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
+
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES products(product_id)
+);
+
+-- Transaction Table
+CREATE TABLE IF NOT EXISTS transactions (
+    transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    payment_method VARCHAR(50),
+    payment_status ENUM('Success', 'Failed') DEFAULT 'Success',
+    transaction_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    FOREIGN KEY (order_id) REFERENCES orders(order_id)
+);
