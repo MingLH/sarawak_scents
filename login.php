1561964bc9a22b814277e3845972c1ce9e62eaff
Isaac <isaacshagal329@gmail.com>
2025-12-30 14:16:40 +0800

    added background with animation in homepage, fade in animation upon loading the page, animated global background for login page

--- login.php
+++ login.php
@@ -51,6 +51,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" /> <title>Login - Sarawak Scents</title>
     <link rel="stylesheet" href="css/style.css" />
 </head>
+
+<body class="moving-bg">
+    <div class="login-container">
+        
 <body>
 
     <a href="index.php" class="back-link">
